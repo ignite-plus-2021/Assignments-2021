58057effacb81241e0826cc019e59f08ad1faa06
technology81 <62051728+technology81@users.noreply.github.com>
2021-03-25 21:24:49 +0530

    Add files via upload

---
+++ MAANASA S PRASAD/Question 8.java
@@ -0,0 +1,97 @@
+package com.company;
+
+public class Main {
+    public void sort(int arr[])
+    {
+        int n = arr.length;
+
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+
+        for (int i = n - 1; i > 0; i--)
+        {
+
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+
+            heapify(arr, i, 0);
+        }
+    }
+
+
+    void heapify(int arr[], int n, int i)
+    {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+
+            heapify(arr, n, largest);
+        }
+    }
+
+
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+    static void bubbleSort(int[] arr)
+    {
+        int temp = 0;
+        int n = arr.length;
+        for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+                if(arr[j-1] > arr[j]){
+                    //swap elements
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+
+            }
+        }
+    }
+
+
+        public static void main(String args[])
+    {
+        int arr[] = { 12, 11, 13, 5, 6, 7 };
+        int n = arr.length;
+        System.out.println("Array Before Sorting");
+        for(int i=0; i < arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        Main ob = new Main();
+        ob.sort(arr);
+
+        System.out.println("Array after Heap sort");
+        printArray(arr);
+        System.out.println("Array After Bubble Sort");
+        for(int i=0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
