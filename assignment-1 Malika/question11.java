52969f4ab96f000540271a84030527e47f87eede
Malikajaiswal <68912714+Malikajaiswal@users.noreply.github.com>
2021-05-03 11:02:12 +0530

    Add files via upload

---
+++ assignment-1 Malika/question11.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Question11 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Enter a decimal number");
+        n = sc.nextInt();
+        String b = Integer.toBinaryString(n);
+        System.out.println("Its binary equivalent is "+ b);
+    }
+}
