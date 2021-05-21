69ece9f94dedca5b80bc5e803a5550b46846958e
dishapalriwala <dishapalriwala2000@gmail.com>
2021-05-21 19:03:11 +0530

    adding exception handling

---
+++ Disha Palriwala/Exception Handling/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+import java.util.Scanner;
+class nonDeliverable extends Exception{
+    public nonDeliverable(String message) {
+        super(message);
+    }
+}
+public class Main {
+
+    public static void zipCode(int zip) throws nonDeliverable {
+        if ((zip == 123) || (zip == 456) || (zip == 789)) {
+            throw new nonDeliverable("Delivery is not available in your area.");
+        } else {
+            System.out.println("Delivery is available in your area.");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your zip code: ");
+        int n = sc.nextInt();
+        try {
+            zipCode(n);
+        } catch (nonDeliverable e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+}
+
+
+
+
